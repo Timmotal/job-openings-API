fcabb322ad75e342153ba43f8b6f016ce042c003
unknown <timmortal44@yahoo.com>
2023-09-01 23:57:13 +0100

    get rate limiter

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ require('express-async-errors');
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
