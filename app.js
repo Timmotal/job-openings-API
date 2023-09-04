156c649737f9f82e34f4531b28298587e27e5067
unknown <timmortal44@yahoo.com>
2023-09-04 23:39:17 +0100

    instantiate connection to DB

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 require('express-async-errors');
-
-// extra security packages
-const helmet = require('helmet');
-const cors = require('cors');
-const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
-
-// Swagger
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-
 const express = require('express');
 const app = express();
+
+// <<<<<<<--EXTRA-SECURITY-PACKAGES-->>>>>>>>>>>
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit') // will need a little bit of configuration
+
+// connectDB
+const connectDB = require('./db/connect')
+
+
+
 
 
