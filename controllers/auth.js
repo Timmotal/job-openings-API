64f27eea8e7b1aaa10e9cb43bd5b485ccf0b4712
unknown <timmortal44@yahoo.com>
2023-09-07 23:57:15 +0100

    import the user schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,5 @@
 import bcrypt from "bcrypt"; // allows for password encryption
-import jwt from "jsonwebtoken"; 
+import jwt from "jsonwebtoken"; // sends a user a web token they will use for authorization
+import User from "../models/User.js"; // Model -> how the data will be structured in MongoDB
+
+/* REGISTER USER */
