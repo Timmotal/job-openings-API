868a78e4befbb9f3abcf7fd7362189e88ec2f6b2
unknown <timmortal44@yahoo.com>
2023-09-11 23:49:16 +0100

    get error middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,5 @@
 const User =  require('../models/User')
 const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+// const bcrypt = require('bcryptjs') refactored out to mongoose error (User.js)
+
