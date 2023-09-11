9d03bb26feed214f31691459934407d21cc00864
unknown <timmortal44@yahoo.com>
2023-09-11 23:50:01 +0100

    create user object from req body

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,8 +11,29 @@ const register = async (req, res) => {
     // CREATE TEMPORARY OBJECT ---->>>>>>>>>>
     // const { name, email, password } = req.body
     // // salt essentially means random bytes, the higher the number, more processing power required and security
-    // con
+    // const salt = await bcrypt.genSalt(10)
+    // const hashedPassword = await bcrypt.hash(password, salt)
+    // // ES6 syntax, name is equal to name
+    // const tempUser = { name, email, password:hashedPassword }
+    // // THERE WILL BE SOME CASES WHERE WE HAVE TO SETUP CHECKING IN THE CONTROLLERS
+    // // const { name, email, password } = req.body
+    // // if (!name || !email || !password ) {
+    // //     throw new BadRequestError(' will you provide a name eh, PO box electric and hubaloo')
+    // // }
+    // // since we want mongoose to do all the validation, we simply pass in the body
+    // // const user = await User.create({...req.body}) //refactored 
+    // // why use a rest operator here -> to collect the arguments and dump them as an array (unsure)
+    // const user = await User.create({...tempUser})
+    // <<<<<<<<<END OF REFACTOR TO MONGOOSE>>>>>>>>>>>>
 
+    // res.send('Register Aug 24th 1638 hours 2023')
+    // res.status(StatusCodes.CREATED).json(req.body) // 201
+    
+    const user = await User.create({ ...req.body }) //create an object here,
+    // console.log('before the beginiing') beginning of the ---> BUG
+
+
+    // WE TOOK THIS OUT AND FOR WHAT?
     // res
     // .status(StatusCodes.CREATED)
     // // says we can generate the token using the instance method instead
