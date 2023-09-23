4e948f89df8f7a0cfc4cabe33052949426b0cc83
unknown <timmortal44@yahoo.com>
2023-09-23 23:44:02 +0100

    find all jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -4,7 +4,11 @@ const { BadRequestError, NotFoundError } = require('../errors')
 
 // CRUD Functionality
 const getAllJobs = async (req, res) => {
-
+    // that user property is going to be in every request, since in the app.js
+    // we place the auth middleware -> infront of all of our jobs route
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt') // feel this once more -> avoid IDs
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length  })
+    // res.send('get all tha frigging jobs')
 }
 
 
