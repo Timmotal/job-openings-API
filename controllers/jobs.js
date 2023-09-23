3a233bb7e0573ff6b36513ff54761f638911c5fb
unknown <timmortal44@yahoo.com>
2023-09-23 23:43:21 +0100

    make get all jobs controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,3 +2,12 @@ const Job = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
+// CRUD Functionality
+const getAllJobs = async (req, res) => {
+
+}
+
+
+
+
+
