23b539ab2527c2a7cca3faa85e480ad7bc42e7b2
unknown <timmortal44@yahoo.com>
2023-10-03 23:56:49 +0100

    and the params the job ID

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -75,7 +75,8 @@ const updateJob = async (req, res) => {
 
 const deleteJob = async (req, res) => {
     const { // we are looking for the user and params
-        user: { userId },ming from the params. says it is important we understand that
+        user: { userId }, // this is coming from the auth middleware
+     params: { id: jobId } // coming from the params. says it is important we understand that
     } = req
  
  
