4adc53ed511d4b707e998d0e1d394ffc6adb0571
unknown <timmortal44@yahoo.com>
2023-10-15 20:58:21 +0100

    include the error for this file and export

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -4,6 +4,8 @@ const CustomAPIError = require('./custom-api');
 class BadRequestError extends CustomAPIError {
   constructor(message) {
     super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
   }
 }
 
+module.exports = BadRequestError;
