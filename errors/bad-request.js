e2b957349fef323c573d20becaed750b480e9c48
unknown <timmortal44@yahoo.com>
2023-10-15 20:57:57 +0100

    make the constructor and super

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -2,6 +2,8 @@ const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
 class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
   }
 }
 
