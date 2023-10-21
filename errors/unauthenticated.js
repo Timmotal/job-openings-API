1fb428d570264444b74df1102814d08acbde69c7
unknown <timmortal44@yahoo.com>
2023-10-21 23:50:04 +0100

    makrf ig fo

--- errors/unauthenticated.js
+++ errors/unauthenticated.js
@@ -4,6 +4,7 @@ const CustomAPIError = require('./custom-api');
 class UnauthenticatedError extends CustomAPIError {
   constructor(message) {
     super(message);
+    this.statusCode = StatusCodes.UNAUTHORIZED;
   }
 }
 
