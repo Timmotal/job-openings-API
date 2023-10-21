00a7f84b4ec75a4d1fc14ec0f360fb5795611440
unknown <timmortal44@yahoo.com>
2023-10-21 23:49:46 +0100

    construct it for shipment

--- errors/unauthenticated.js
+++ errors/unauthenticated.js
@@ -2,6 +2,8 @@ const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
 class UnauthenticatedError extends CustomAPIError {
-}
+  constructor(message) {
+    super(message);
+  }
 }
 
