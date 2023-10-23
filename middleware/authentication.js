86e9f79ef048141a0581018ecfb3c3257ff61118
unknown <timmortal44@yahoo.com>
2023-10-23 23:09:10 +0100

    check if headers are correct

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,6 +4,11 @@ const { UnauthenticatedError } = require('../errors')// there is an index file, 
 
 // a middleware
 const auth = async (req, res, next) => {
+    // check header
+    const authHeader = req.headers.authorization
+
+    // says some setups do not have "spaces" and it doesn't really matter (07:54:44)
+    // but how do these setups verify the token jumbled together with 'Bearer'
   
 
 
