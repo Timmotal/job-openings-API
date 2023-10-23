cc17c0586981fe396d24aad6e9b072002e9e2413
unknown <timmortal44@yahoo.com>
2023-10-23 23:08:56 +0100

    make the auth func

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,5 +3,10 @@ const jwt = require('jsonwebtoken')
 const { UnauthenticatedError } = require('../errors')// there is an index file, no need to specify
 
 // a middleware
+const auth = async (req, res, next) => {
+  
+
+
+}
 
 
