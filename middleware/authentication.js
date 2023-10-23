298f2541b8a799771411d3274de4b3566c377e98
unknown <timmortal44@yahoo.com>
2023-10-23 23:08:43 +0100

    and unautherror for dealing with errors

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,4 +1,7 @@
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')// there is an index file, no need to specify
+
+// a middleware
 
 
