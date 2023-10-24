156608c6888520b720cab656c0a1f3ee5ab319f7
unknown <timmortal44@yahoo.com>
2023-10-24 23:40:22 +0100

    verify JWT

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -15,6 +15,26 @@ const auth = async (req, res, next) => {
 // we split this into an  array and then we want the second items
     const token = authHeader.split(' ')[1]
 
+    try {
+        // we are getting the payload that we are setting up in the route (auth) but how so?
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        // attach the user to the job routes
+        req.user = { userId: payload.userId, name: payload.name} // we create an object here
+        // we need to invoke the next () otherwise we wont get to the job route
+        next()
+         
+        //  <<<<-----a SYNTAX YOU MIGHT come ACROSS--(08:06:00)----->>>>>>>>
+        // where instead of creating a object -> they look for the user in the database
+        // so they take a model and findByID or  findOne and pass in the ID coming from the token
+        // in most cases they would use select to remove the password
+        // no point to pass the password to the upcoming middleware
+        // they then set (req.user) equal to the "user"
+        // says HE DIDNT go with this because -> 
+        // we really have no functionality to remove the user anyways
+        // so if he gets the ID from the token -> he is really sure there is a user on the other-side
+        // const user = User.findById(payload.id).select('-password')
+        // req.user = user
+    }
 }
 
 
