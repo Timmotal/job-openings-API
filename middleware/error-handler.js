61aa60c70198a0d40bd296fc380175983b8dd2f4
unknown <timmortal44@yahoo.com>
2023-10-30 22:14:09 +0000

    make the func

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,15 @@
 // const { CustomAPIError } = require('../errors') // we removed this
 const { StatusCodes } = require('http-status-codes')
+const errorHandlerMiddleware = (err, req, res, next) => {
+  // we have this customError one and essentially we are looking for the statusCode property
+  // on the err object coming in (just above, in the params)
+  // cos remember we are setting up A new one -> customError
+  // and the same goes for message
+
+  // because we know we will be manipulating those values
+ 
+  
+
 
 }
 
