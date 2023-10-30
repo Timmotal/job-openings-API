71e881cc04a2f6574602d9f23f03f789b423628d
unknown <timmortal44@yahoo.com>
2023-10-30 22:14:22 +0000

    init custom erro

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,7 +7,21 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // and the same goes for message
 
   // because we know we will be manipulating those values
- 
+  let customError = {
+    // set default -> if there is already something provided -> send back that, if not then the default
+    // there will be multiple IFs that will manipulate this value
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    // says if in the error, we already have the statusCodes,
+  //  then use that, then we will setup the generic response
+    msg:err.message || 'Something went wrong, will you try again later'
+  }
+  // console.log(err)
+
+  // if (err instanceof CustomAPIError) { //Error Classes
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+
+  
   
 
 
