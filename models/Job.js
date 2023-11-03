8243f7a7dc8bf1009221630e84561ad01b8c3d2f
unknown <timmortal44@yahoo.com>
2023-11-03 21:22:27 +0000

    setup company are

--- models/Job.js
+++ models/Job.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
 const JobSchema = new mongoose.Schema(
-  
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxlength: 50,
+    },
    
    
   
+  },
 );
 
 
