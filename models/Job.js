11b17454286efaca6b1dd3c396b7a2af20b1d145
unknown <timmortal44@yahoo.com>
2023-11-03 21:22:35 +0000

    and the postion

--- models/Job.js
+++ models/Job.js
@@ -7,7 +7,11 @@ const JobSchema = new mongoose.Schema(
       required: [true, 'Please provide company name'],
       maxlength: 50,
     },
-   
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
+    },
    
   
   },
