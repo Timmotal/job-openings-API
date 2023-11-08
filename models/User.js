3833c07d462296dc1614ceca5f48fddf2ae49eb9
unknown <timmortal44@yahoo.com>
2023-11-08 22:28:04 +0000

    have the required implemented

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,       
+        required: [true, 'you dont have a name eh?'],
     },
    
 
