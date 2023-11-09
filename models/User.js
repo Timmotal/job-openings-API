28702d781399909bb09f68ee95fe3bf657b486b3
unknown <timmortal44@yahoo.com>
2023-11-09 21:11:09 +0000

    use regex to check

--- models/User.js
+++ models/User.js
@@ -12,7 +12,10 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'vamos if you no get email'],
-    
+        match: [
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          'non valid email are excused',
+        ],
 
         
     },
