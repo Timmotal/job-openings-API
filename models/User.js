197eba08ee126dd6f5258390eb2241ade58c5e68
unknown <timmortal44@yahoo.com>
2023-11-16 22:02:53 +0000

    another JWT

--- models/User.js
+++ models/User.js
@@ -52,11 +52,9 @@ const UserSchema = new mongoose.Schema({
 // }
 
 UserSchema.methods.createJWT = function () {
-    // remember -> in the function, we can access the document, by using the below
-    // (I think I meant to say using "this")
-    return jwt.sign(
-        { userId: this._id, name: this.name },
-         process.env.JWT_SECRET,
-    
-}
+  
+  }
+  
+ 
+  
 
