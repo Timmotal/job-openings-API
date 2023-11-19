1a5d8ba2fa13834fd5e8d739a9f13ea215e7eb6e
unknown <timmortal44@yahoo.com>
2023-11-19 21:54:07 +0000

    gett all the job from controlers

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,4 +1,12 @@
 const express = require('express')
+
 const router = express.Router()
+const {
+  createJob,
+  deleteJob,
+  getAllJobs,
+  updateJob,
+  getJob,
+} = require('../controllers/jobs')
 
 
